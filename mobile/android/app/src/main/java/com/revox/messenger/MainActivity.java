117ba5e4b24e12cc829c4c3124cb80983e1433de
Muslim <muslimborz.salamov@gmail.com>
2026-07-11 23:37:34 +0300

    Before Compose setup

--- mobile/android/app/src/main/java/com/revox/messenger/MainActivity.java
+++ mobile/android/app/src/main/java/com/revox/messenger/MainActivity.java
@@ -2,8 +2,10 @@ package com.revox.messenger;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.view.Gravity;
+import android.widget.*;
 
 public class MainActivity extends Activity {
 
@@ -11,14 +13,51 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView text = new TextView(this);
+        LinearLayout root = new LinearLayout(this);
+        root.setOrientation(LinearLayout.VERTICAL);
+        root.setGravity(Gravity.CENTER);
+        root.setPadding(40, 40, 40, 40);
+        root.setBackgroundColor(Color.rgb(10,10,10));
 
-        text.setText("RevOx\n\nДобро пожаловать");
-        text.setTextSize(28);
-        text.setTextColor(Color.WHITE);
-        text.setBackgroundColor(Color.BLACK);
-        text.setGravity(17);
 
-        setContentView(text);
+        TextView logo = new TextView(this);
+        logo.setText("🦆");
+        logo.setTextSize(60);
+        logo.setGravity(Gravity.CENTER);
+
+
+        TextView title = new TextView(this);
+        title.setText("RevOx");
+        title.setTextSize(36);
+        title.setTextColor(Color.WHITE);
+        title.setTypeface(Typeface.DEFAULT, Typeface.BOLD);
+        title.setGravity(Gravity.CENTER);
+
+
+        EditText email = new EditText(this);
+        email.setHint("Email");
+        email.setTextColor(Color.WHITE);
+        email.setHintTextColor(Color.GRAY);
+        email.setInputType(33);
+
+
+        Button button = new Button(this);
+        button.setText("Continue");
+
+
+        root.addView(logo);
+        root.addView(title);
+
+        root.addView(email,
+            new LinearLayout.LayoutParams(
+                -1,
+                120
+            )
+        );
+
+        root.addView(button);
+
+
+        setContentView(root);
     }
 }
