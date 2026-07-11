2971fd29bc47b47a88569fb1bdf377892f449a16
Muslim <muslimborz.salamov@gmail.com>
2026-07-11 23:06:10 +0300

    Native Android MainActivity

--- mobile/android/app/src/main/java/com/revox/messenger/MainActivity.java
+++ mobile/android/app/src/main/java/com/revox/messenger/MainActivity.java
@@ -1,5 +1,24 @@
 package com.revox.messenger;
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.graphics.Color;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView text = new TextView(this);
+
+        text.setText("RevOx\n\nДобро пожаловать");
+        text.setTextSize(28);
+        text.setTextColor(Color.WHITE);
+        text.setBackgroundColor(Color.BLACK);
+        text.setGravity(17);
+
+        setContentView(text);
+    }
+}
