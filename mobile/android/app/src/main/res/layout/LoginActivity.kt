db695582f8a957de5d92e6fd66ffe7ecfec4a1db
Muslim <muslimborz.salamov@gmail.com>
2026-07-13 23:48:07 +0300

    Clean android project structure

---
+++ mobile/android/app/src/main/res/layout/LoginActivity.kt
@@ -0,0 +1,39 @@
+package com.revox.messenger.activities
+
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.revox.messenger.R
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var emailInput: EditText
+    private lateinit var continueButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+
+        emailInput = findViewById(R.id.emailInput)
+        continueButton = findViewById(R.id.continueButton)
+
+        continueButton.setOnClickListener {
+
+            val email = emailInput.text.toString().trim()
+
+            if(email.isEmpty()){
+                emailInput.error = "Введите email"
+                return@setOnClickListener
+            }
+
+            Toast.makeText(
+                this,
+                "Email: $email",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            // тут дальше подключим API
+        }
+    }
+}
