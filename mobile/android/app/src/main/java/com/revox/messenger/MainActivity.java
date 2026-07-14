5844f4f19b3ff4101790a111ac3172eb061ef5d0
Muslim <muslimborz.salamov@gmail.com>
2026-07-15 02:59:37 +0300

    Use Capacitor WebView instead of native test screen

--- mobile/android/app/src/main/java/com/revox/messenger/MainActivity.java
+++ mobile/android/app/src/main/java/com/revox/messenger/MainActivity.java
@@ -1,24 +1,6 @@
 package com.revox.messenger;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
-import android.graphics.Color;
-import android.view.Gravity;
+import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        TextView text = new TextView(this);
-        text.setText("RevOx\n\nNative Android");
-        text.setTextSize(30);
-        text.setTextColor(Color.WHITE);
-        text.setBackgroundColor(Color.BLACK);
-        text.setGravity(Gravity.CENTER);
-
-        setContentView(text);
-    }
+public class MainActivity extends BridgeActivity {
 }
