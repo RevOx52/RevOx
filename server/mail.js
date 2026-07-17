7088985b631d6e0560bb28fa3348c605ce2e83c5
Muslim <muslimborz.salamov@gmail.com>
2026-07-17 13:09:29 +0300

    Fix SMTP IPv4 connection

--- server/mail.js
+++ server/mail.js
@@ -5,33 +5,41 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
 
-host:process.env.SMTP_HOST,
+    host: process.env.SMTP_HOST,
 
-port:process.env.SMTP_PORT,
+    port: 587,
 
-secure:false,
+    secure: false,
 
-auth:{
-user:process.env.SMTP_USER,
-pass:process.env.SMTP_PASS
-}
+    family: 4,
 
-});
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    },
 
-
-module.exports = async function sendCode(email,code){
-
-await transporter.sendMail({
-
-from:process.env.SMTP_USER,
-
-to:email,
-
-subject:"RevOx verification code",
-
-text:
-`Your RevOx code: ${code}`
+    tls: {
+        rejectUnauthorized: false
+    }
 
 });
+
+
+module.exports = async function sendCode(email, code){
+
+
+    await transporter.sendMail({
+
+        from: `"RevOx" <${process.env.SMTP_USER}>`,
+
+        to: email,
+
+        subject: "RevOx verification code",
+
+        text:
+        `Your RevOx code: ${code}`
+
+    });
+
 
 };
